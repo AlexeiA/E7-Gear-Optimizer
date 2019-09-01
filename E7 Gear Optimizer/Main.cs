fae18804de85329e9f7879aa5cf5d1853d9cf818
AlexeiA <alexei.a3@gmail.com>
2019-09-01 13:45:44 +0300

    - fixed small problem in updateItemList()
    Order of items can change due to change of sortColumn values, so restore selected row by item's id instead of current cell's Y

--- E7 Gear Optimizer/Main.cs
+++ E7 Gear Optimizer/Main.cs
@@ -305,9 +305,10 @@ namespace E7_Gear_Optimizer
             l_ItemCount.Text = filteredList.Count().ToString();
             //restore previous sorting and select previously selected cell
             if (order != SortOrder.None) dgv_Inventory.Sort(sortColumn, (ListSortDirection)Enum.Parse(typeof(ListSortDirection), order.ToString()));
+            //order of items can change due to change of sortColumn values, so restore selected row by item id
             if (cell.X > -1 && cell.Y > -1 && cell.X < dgv_Inventory.ColumnCount && cell.Y < dgv_Inventory.RowCount)
             {
-                dgv_Inventory.CurrentCell = dgv_Inventory.Rows[cell.Y].Cells[cell.X];
+                dgv_Inventory.CurrentCell = dgv_Inventory.Rows.Cast<DataGridViewRow>().Where(x => x.Cells["c_ItemID"].Value.ToString() == ID).First().Cells[cell.X];
             }
         }
 
